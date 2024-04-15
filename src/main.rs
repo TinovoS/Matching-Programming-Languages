e88c5d5f0f3f1700b1d2239493175903045d8259
Dejan43 <148759099+Dejan43@users.noreply.github.com>
2024-04-15 15:22:58 +0200

    Update main.rs
    Added function for randomization

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,18 @@ use iced::executor;
 use iced::{Application, Command, Element, Settings, Theme,Length};
 use iced::widget::{Column,Row,Image};
 
+//Structure with needed information for memory game
+struct GameState{
+    num_of_cards: u8,
+    picked: [bool; 6],
+    correct_order: [i8; 6],
+    card_order: [i8; 6],
+    picked_count: u8,
+    pick_number: u8,
+    correct_pairs: u8,
+    card: i8,
+    reset: bool,
+}
 
 pub fn main() -> iced::Result {
     Hello::run(Settings::default())
@@ -28,9 +40,25 @@ impl Application for Hello {
     }
 
     fn view(&self) -> Element<Self::Message> {
+
         let image_width = Length::Fill; // Each image takes 1/3 of the width
         let image_height = image_width; // Maintain aspect ratio
 
+        //lets initialize game status
+        //------------------------------------
+        let game_state = GameState{
+            num_of_cards: 6,
+            picked: [true; 6],
+            correct_order: [-1; 6],
+            card_order: [-1; 6],
+            picked_count: 0,
+            pick_number: 0,
+            correct_pairs: 0,
+            card: 0,
+            reset: false,
+        };
+        randomize_cards(game_state);
+        //------------------------------------
         Column::new()
             .spacing(10) // Add spacing between rows (optional)
             .push(
@@ -73,4 +101,22 @@ impl Application for Hello {
             )
             .into()
     }
+}
+
+fn randomize_cards(mut game_state: GameState) -> () {
+    for n in 0..6 {
+        game_state.card_order[n] = -1;
+    }
+    for n in 0..6 {
+        let mut x = rand::random::<i8>() % 6;
+        while !game_state.card_order.contains(&x) {
+            x = rand::random::<i8>() % 6;
+        }
+        game_state.card_order[n] = x as i8;
+        game_state.picked[n] = false;
+
+    }
+    game_state.pick_number = 0;
+    game_state.correct_pairs = 0;
+    game_state.card = -1;
 }
